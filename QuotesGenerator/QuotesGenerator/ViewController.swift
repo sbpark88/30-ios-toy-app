220dec357b3cc68ce90ccd042427b00cd8c98b22
sbpark88 <saebyulpark88@gmail.com>
2023-05-03 23:07:58 +0900

    Add: Famous Saying

---
+++ QuotesGenerator/QuotesGenerator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuotesGenerator
+//
+//  Created by 박새별 on 2023/05/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
