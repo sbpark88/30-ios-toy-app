674ef591c18b2ce93c1ec0252e7356449cc8c240
sbpark88 <saebyulpark88@gmail.com>
2023-05-16 05:18:39 +0900

    Add: Calculator with default layout

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by 박새별 on 2023/05/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
