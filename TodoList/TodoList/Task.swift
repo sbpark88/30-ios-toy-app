658048b49fb72e5579f723eb8aac5df1f597a94e
sbpark88 <saebyulpark88@gmail.com>
2023-06-01 08:26:24 +0900

    Feat: Add new Todo with 'UserDefaults'

---
+++ TodoList/TodoList/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  TodoList
+//
+//  Created by 박새별 on 2023/06/01.
+//
+
+import Foundation
+
+struct Task {
+    var title: String
+    var done: Bool = false
+}
--- TodoList/TodoList/ViewController.swift
+++ TodoList/TodoList/ViewController.swift
@@ -9,11 +9,80 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    var tasks: [Task] = [Task]() {
+        didSet {
+            self.saveTasks()
+        }
     }
 
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.dataSource = self
+        self.loadTasks()
+    }
+
+    @IBAction func tapEditButton(_ sender: UIBarButtonItem) {
+
+    }
+
+    @IBAction func tapAddButton(_ sender: UIBarButtonItem) {
+        var inputText = UITextField()
+
+        let alert = UIAlertController(title: "할 일 등록", message: "할 일을 등록해주세요.", preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.placeholder = "할 일을 입력해주세요."
+            inputText = textField
+        }
+
+        let registerButton = UIAlertAction(title: "등록", style: .default) { [weak self] _ in
+            guard let newTodo = inputText.text, !newTodo.isEmpty else { return }
+            let task = Task(title: newTodo)
+            self?.tasks.append(task)
+            self?.saveTasks()
+            self?.tableView.reloadData()
+        }
+        let cancelButton = UIAlertAction(title: "취소", style: .cancel)
+        alert.addAction(registerButton)
+        alert.addAction(cancelButton)
+
+        present(alert, animated: true)
+    }
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tasks.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row].title
+        return cell
+    }
+}
+
+extension ViewController {
+    func saveTasks() {
+        let data = self.tasks.map {
+            [
+                "title": $0.title,
+                "done": $0.done
+            ] as [String: Any]
+        }
+        let userDefaults = UserDefaults.standard
+        userDefaults.set(data, forKey: "tasks")
+    }
+
+    func loadTasks() {
+        let userDefaults = UserDefaults.standard
+        guard let data = userDefaults.object(forKey: "tasks") as? [[String: Any]] else { return }
+        self.tasks = data.compactMap {
+            guard let title = $0["title"] as? String else { return nil }
+            guard let done = $0["done"] as? Bool else { return nil }
+            return Task(title: title, done: done)
+        }
+    }
+}
