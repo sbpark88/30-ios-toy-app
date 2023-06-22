78eefea15ab88931af6503dfea69052c3d0564ee
sbpark88 <saebyulpark88@gmail.com>
2023-06-22 22:46:58 +0900

    Style: Set default border to UITextView

--- Diary/Diary/VIew/Diary/WriteDiaryViewController.swift
+++ Diary/Diary/VIew/Diary/WriteDiaryViewController.swift
@@ -9,11 +9,27 @@ import UIKit
 
 class WriteDiaryViewController: UIViewController {
 
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentTextView: UITextView!
+    @IBOutlet weak var dateTextField: UITextField!
+    @IBOutlet weak var confirmButton: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configureContentTextView()
+    }
 
-        // Do any additional setup after loading the view.
+    @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
     }
     
+}
 
+// MARK: init
+extension WriteDiaryViewController {
+    private func configureContentTextView() {
+        let borderColor = UIColor(red: 220/255, green: 220/255, blue: 220/255, alpha: 1.0)
+        self.contentTextView.layer.borderColor = borderColor.cgColor
+        self.contentTextView.layer.borderWidth = 0.5
+        self.contentTextView.layer.cornerRadius = 5.0
+    }
 }
