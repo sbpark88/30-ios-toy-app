540d50fdd95d9b3b60adea60ae478bcc35f2ba14
sbpark88 <saebyulpark88@gmail.com>
2023-06-22 18:54:17 +0900

    Add: Diary Detail View

---
+++ Diary/Diary/VIew/Star/DiaryDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DiaryDetailViewController.swift
+//  Diary
+//
+//  Created by 박새별 on 2023/06/22.
+//
+
+import UIKit
+
+class DiaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}
