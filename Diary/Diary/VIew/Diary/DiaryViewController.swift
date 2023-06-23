5c65c817d0ac13ab049108c68d83dbc00a45d377
sbpark88 <saebyulpark88@gmail.com>
2023-06-23 14:34:08 +0900

    Feat: Add DatePicker wheel style

--- Diary/Diary/VIew/Diary/DiaryViewController.swift
+++ Diary/Diary/VIew/Diary/DiaryViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class DiaryViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
--- Diary/Diary/VIew/Diary/WriteDiaryViewController.swift
+++ Diary/Diary/VIew/Diary/WriteDiaryViewController.swift
@@ -14,14 +14,17 @@ class WriteDiaryViewController: UIViewController {
     @IBOutlet weak var dateTextField: UITextField!
     @IBOutlet weak var confirmButton: UIBarButtonItem!
     
+    private let datePicker = UIDatePicker()
+    private var diaryDate: Date?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.configureContentTextView()
+        self.configureDatePicker()
     }
 
     @IBAction func tapConfirmButton(_ sender: UIBarButtonItem) {
     }
-    
 }
 
 // MARK: init
@@ -31,5 +34,20 @@ extension WriteDiaryViewController {
         self.contentTextView.layer.borderColor = borderColor.cgColor
         self.contentTextView.layer.borderWidth = 0.5
         self.contentTextView.layer.cornerRadius = 5.0
+    }
+    
+    private func configureDatePicker() {
+        self.datePicker.datePickerMode = .date
+        self.datePicker.preferredDatePickerStyle = .wheels
+        self.datePicker.addTarget(self, action: #selector(datePickerValueDidChange(_:)), for: .valueChanged)
+        self.dateTextField.inputView = self.datePicker
+    }
+
+    @objc private func datePickerValueDidChange(_ datePicker: UIDatePicker) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy 년 MM 월 dd 일 (EEEEE)" // 2023년 06월 22일 (목)
+        formatter.locale = Locale(identifier: "ko_KR")
+        self.diaryDate = datePicker.date
+        self.dateTextField.text = formatter.string(from: datePicker.date)
     }
 }
