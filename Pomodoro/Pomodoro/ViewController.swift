7a4641279e87846f37ef3df0ef67165e2506e6da
sbpark88 <saebyulpark88@gmail.com>
2023-07-05 19:49:27 +0900

    Update: UI for confirm/cancel button

--- Pomodoro/Pomodoro/ViewController.swift
+++ Pomodoro/Pomodoro/ViewController.swift
@@ -8,12 +8,97 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var cancelButton: UIButton!
+    @IBOutlet weak var confirmButton: UIButton!
+    
+    var countDownSeconds: Int?
+    var timerStatus: TimerStatus = .end
+    var timer: DispatchSourceTimer?
+    var remainTime = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.configureToggleButton()
     }
-
-
+    
+    func setTimerInfoViewVisible(isHidden: Bool) {
+        self.timerLabel.isHidden = isHidden
+        self.progressBar.isHidden = isHidden
+    }
+    
+    func startTimer() {
+        if timer == nil {
+            timer = DispatchSource.makeTimerSource(flags: [], queue: .main)
+            timer?.schedule(deadline: .now(), repeating: 1)
+            timer?.setEventHandler(handler: { [weak self] in
+                self?.remainTime -= 1
+                debugPrint(self?.remainTime as Any)
+                
+                if self?.remainTime ?? 0 <= 0 {
+                    self?.stopTimer()
+                }
+            })
+            timer?.resume()
+        }
+    }
+    
+    func stopTimer() {
+        if timerStatus == .pause {
+            timer?.resume()
+        }
+        timerStatus = .end
+        cancelButton.isEnabled = false
+        setTimerInfoViewVisible(isHidden: true)
+        datePicker.isHidden = false
+        confirmButton.isSelected = false
+        timer?.cancel()
+        timer = nil
+    }
+    
+    @IBAction func tapCancelButton(_ sender: UIButton) {
+        switch self.timerStatus {
+            
+        case .start, .pause:
+            stopTimer()
+        default:
+            break
+        }
+    }
+    
+    @IBAction func tapConfirmButton(_ sender: UIButton) {
+        countDownSeconds = Int(datePicker.countDownDuration)
+        guard let countDownSeconds else { return }
+        
+        switch timerStatus {
+        case .end:
+            remainTime = countDownSeconds
+            timerStatus = .start
+            setTimerInfoViewVisible(isHidden: false)
+            datePicker.isHidden = true
+            confirmButton.isSelected = true
+            cancelButton.isEnabled = true
+            startTimer()
+        case .start:
+            timerStatus = .pause
+            confirmButton.isSelected = false
+            timer?.suspend()
+        case .pause:
+            timerStatus = .start
+            confirmButton.isSelected = true
+            timer?.resume()
+        }
+    }
 }
 
+// MARK: Config
+extension ViewController {
+    func configureToggleButton() {
+        self.confirmButton.setTitle("시작", for: .normal)
+        self.confirmButton.setTitle("일시정지", for: .selected)
+    }
+}
