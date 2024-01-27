3c7acd8c7f1e59239dea04c881a3423f87c1cbe9
sbpark88 <saebyulpark88@gmail.com>
2024-01-27 22:51:43 +0900

    Add: Notice App

---
+++ Notice/Notice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notice
+//
+//  Created by 박새별 on 1/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
